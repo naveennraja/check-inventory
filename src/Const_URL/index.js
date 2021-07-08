45ddd171e675e51d3036bce054e5b8c0b65b4cba
Naveen Nata Raja <naveennraja@gmail.com>
2021-07-08 09:49:37 +0100

    Check Inventory  Repo check

--- src/Const_URL/index.js
+++ src/Const_URL/index.js
@@ -1,3 +1,3 @@
 export const INVENTORY_URL =
-  'https://run.mocky.io/v3/41371f37-7ad4-4029-8fa3-040338a08d85';
+  'https://react-challenge-api.azurewebsites.net/vehicles';
 export const CHECK_INVENTORY_DB = 'checkInventory';
